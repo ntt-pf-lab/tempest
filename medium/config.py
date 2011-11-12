e7c9fc09568be853cea291cd081a52f27d1e8a2d
Nachi Ueno <ueno.nachi@lab.ntt.co.jp>
2011-11-13 04:27:27 +0900

    add a glance daemon process during test

--- medium/config.py
+++ medium/config.py
@@ -19,6 +19,16 @@ class GlanceConfig(object):
         """Directory of Glance home. Defaults to /opt/stack/glance"""
         return self.get("directory", "/opt/stack/glance")
 
+    @property
+    def registry_config(self):
+        """Path to Glance registry config. Defaults to etc/glance-registry.conf"""
+        return self.get("registry_config", "etc/glance-registry.conf")
+
+    @property
+    def api_config(self):
+        """Path to Glance api config. Defaults to etc/glance-api.conf"""
+        return self.get("api_config", "etc/glance-api.conf")
+
 
 class MediumConfig(config.StormConfig):
     def __init__(self, path="etc/medium.conf"):
