7d6c48c1e4d4fd12c4f9a1501903c4a7d17ce3c2
Nachi Ueno <ueno.nachi@lab.ntt.co.jp>
2011-11-24 15:08:45 +0900

    pep8 to merge correct

--- storm/services/nova/json/images_client.py
+++ storm/services/nova/json/images_client.py
@@ -12,12 +12,13 @@ class ImagesClient(object):
             config = storm.config.StormConfig()
         self.config = config
 
-        self.client = rest_client.RestClient(username, key, auth_url, tenant_name,
+        self.client = rest_client.RestClient(username, key,
+                                             auth_url, tenant_name,
                                              config=config)
         self.build_interval = self.config.nova.build_interval
         self.build_timeout = self.config.nova.build_timeout
 
-    def create_image(self, server_id, name, meta = None):
+    def create_image(self, server_id, name, meta=None):
         """
         Creates an image of the original server.
         """
@@ -32,47 +33,47 @@ class ImagesClient(object):
             post_body['metadata'] = meta
 
         post_body = json.dumps(post_body)
-        resp, body = self.client.post('servers/%s/action' % 
+        resp, body = self.client.post('servers/%s/action' %
                                       str(server_id), post_body)
         # Normal response has no content.
         # XXX duplicate of servers_client.create_image
         if int(resp['content-length']) > 0:
             body = json.loads(body)
         return resp, body
-        
+
     def list_images(self, params=None):
         url = 'images'
         if params != None:
             param_list = []
             for param, value in params.iteritems():
                 param_list.append("%s=%s&" % (param, value))
-                
+
             url = "images?" + "".join(param_list)
-        
+
         resp, body = self.client.get(url)
         body = json.loads(body)
         return resp, body
-        
+
     def list_images_with_detail(self, params=None):
         url = 'images/detail'
         if params != None:
             param_list = []
             for param, value in params.iteritems():
                 param_list.append("%s=%s&" % (param, value))
-                
+
             url = "images/detail?" + "".join(param_list)
-        
+
         resp, body = self.client.get(url)
         body = json.loads(body)
         return resp, body
-        
+
     def get_image(self, image_id):
         resp, body = self.client.get("images/%s" % str(image_id))
         body = json.loads(body)
         if resp['status'] == '404':
             raise exceptions.ItemNotFoundException(body['itemNotFound'])
         return resp, body['image']
-        
+
     def delete_image(self, image_id):
         return self.client.delete("images/%s" % str(image_id))
 
