ad5ee54d240d478b6a904e6eff8548a9fd23fe50
Nachi Ueno <ueno.nachi@lab.ntt.co.jp>
2011-11-30 16:07:57 +0900

    test for raising error during defineXML of libvirt

---
+++ medium/tests/libvirt-fakes/launch-error/libvirt/__init__.py
@@ -0,0 +1,40 @@
+import os
+
+
+VIR_CRED_AUTHNAME = object()
+VIR_CRED_NOECHOPROMPT = object()
+
+
+class libvirtError(Exception):
+    pass
+
+
+def openAuth(uri, auth, n):
+    class fake(object):
+        @staticmethod
+        def defineXML(xml):
+            raise libvirtError
+
+        @staticmethod
+        def listDomainsID():
+            return []
+
+        @staticmethod
+        def getCapabilities():
+            return file(os.path.join(
+                os.path.dirname(os.path.dirname(__file__)),
+                'capabilities.xml')).read()
+
+        @staticmethod
+        def getType():
+            return 'qemu'
+
+        @staticmethod
+        def getVersion():
+            return '12003'
+
+        @staticmethod
+        def nwfilterDefineXML(xml):
+            pass
+
+    return fake
---
+++ medium/tests/test_opposite_illegal_state.py
@@ -0,0 +1,166 @@
+import os
+import subprocess
+import time
+
+import unittest2 as unittest
+from nose.plugins.attrib import attr
+
+from storm import openstack
+import storm.config
+from storm import exceptions
+from storm.common.utils.data_utils import rand_name
+
+from medium.tests.processes import (
+        GlanceRegistryProcess, GlanceApiProcess,
+        KeystoneProcess,
+        NovaApiProcess, NovaComputeProcess,
+        NovaNetworkProcess, NovaSchedulerProcess)
+
+
+config = storm.config.StormConfig('etc/medium-less-build_timeout.conf')
+environ_processes = []
+
+
+def setUpModule(module):
+    environ_processes = module.environ_processes
+    config = module.config
+
+    # glance.
+    environ_processes.append(GlanceRegistryProcess(
+            config.glance.directory,
+            config.glance.registry_config))
+    environ_processes.append(GlanceApiProcess(
+            config.glance.directory,
+            config.glance.api_config,
+            config.glance.host,
+            config.glance.port))
+
+    # keystone.
+    environ_processes.append(KeystoneProcess(
+            config.keystone.directory,
+            config.keystone.config,
+            config.keystone.host,
+            config.keystone.port))
+
+    for process in environ_processes:
+        process.start()
+    time.sleep(10)
+
+
+def tearDownModule(module):
+    for process in module.environ_processes:
+        process.stop()
+    del module.environ_processes[:]
+
+
+class FunctionalTest(unittest.TestCase):
+
+    config = config
+
+    def setUp(self):
+        self.os = openstack.Manager(config=self.config)
+        self.image_ref = self.config.env.image_ref
+        self.flavor_ref = self.config.env.flavor_ref
+        self.ss_client = self.os.servers_client
+        self.img_client = self.os.images_client
+        self.testing_processes = []
+
+        # nova.
+        self.testing_processes.append(NovaApiProcess(
+                self.config.nova.directory,
+                self.config.nova.host,
+                self.config.nova.port))
+        self.testing_processes.append(NovaNetworkProcess(
+                self.config.nova.directory))
+        self.testing_processes.append(NovaSchedulerProcess(
+                self.config.nova.directory))
+
+        # reset db.
+        subprocess.check_call('mysql -u%s -p%s -e "'
+                              'DROP DATABASE IF EXISTS nova;'
+                              'CREATE DATABASE nova;'
+                              '"' % (
+                                  self.config.mysql.user,
+                                  self.config.mysql.password),
+                              shell=True)
+        subprocess.call('bin/nova-manage db sync',
+                        cwd=self.config.nova.directory, shell=True)
+
+        for process in self.testing_processes:
+            process.start()
+        time.sleep(10)
+
+        # create users.
+        subprocess.check_call('bin/nova-manage user create '
+                              '--name=admin --access=secrete --secret=secrete',
+                              cwd=self.config.nova.directory, shell=True)
+
+        # create projects.
+        subprocess.check_call('bin/nova-manage project create '
+                              '--project=1 --user=admin',
+                              cwd=self.config.nova.directory, shell=True)
+
+        """
+        # allocate networks.
+        subprocess.check_call('bin/nova-manage network create '
+                              '--label=private_1-1 '
+                              '--project_id=1 '
+                              '--fixed_range_v4=10.0.0.0/24 '
+                              '--bridge_interface=br-int '
+                              '--num_networks=1 '
+                              '--network_size=32 ',
+                              cwd=self.config.nova.directory, shell=True)
+        subprocess.check_call('bin/nova-manage network create '
+                              '--label=private_1-2 '
+                              '--project_id=1 '
+                              '--fixed_range_v4=10.0.1.0/24 '
+                              '--bridge_interface=br-int '
+                              '--num_networks=1 '
+                              '--network_size=32 ',
+                              cwd=self.config.nova.directory, shell=True)
+        """
+
+    def tearDown(self):
+        # kill still existing virtual instances.
+        for line in subprocess.check_output('virsh list --all',
+                                            shell=True).split('\n')[2:-2]:
+            (id, name, state) = line.split()
+            if state == 'running':
+                subprocess.check_call('virsh destroy %s' % id, shell=True)
+            subprocess.check_call('virsh undefine %s' % name, shell=True)
+
+        for process in self.testing_processes:
+            process.stop()
+        del self.testing_processes[:]
+
+    def get_fake_libvirt_path(self, name):
+        return os.path.join(
+                os.path.dirname(__file__),
+                'libvirt-fakes',
+                name)
+
+
+class LibvirtLaunchErrorTest(FunctionalTest):
+    def setUp(self):
+        super(LibvirtLaunchErrorTest, self).setUp()
+        compute = NovaComputeProcess(self.config.nova.directory)
+        compute.env = os.environ.copy()
+        compute.env['PYTHONPATH'] = self.get_fake_libvirt_path('launch-error')
+        compute.start()
+        self.testing_processes.append(compute)
+
+    @attr(kind='medium')
+    def test_through(self):
+        accessIPv4 = '1.1.1.1'
+        accessIPv6 = '::babe:220.12.22.2'
+        name = rand_name('server')
+        resp, server = self.ss_client.create_server(name,
+                                                    self.image_ref,
+                                                    self.flavor_ref,
+                                                    accessIPv4=accessIPv4,
+                                                    accessIPv6=accessIPv6)
+
+        # Wait for the server to become ERROR.BUILD
+        self.assertRaises(exceptions.BuildErrorException,
+                          self.ss_client.wait_for_server_status,
+                          server['id'], 'ERROR')
