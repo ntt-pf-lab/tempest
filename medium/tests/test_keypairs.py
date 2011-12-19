c5b1e23b584e36114d7da1cbcf510756f36487c3
Nachi Ueno <ueno.nachi@lab.ntt.co.jp>
2011-12-19 11:14:04 +0900

    merge di testmodule

--- medium/tests/test_keypairs.py
+++ medium/tests/test_keypairs.py
@@ -42,44 +42,43 @@ environ_processes = []
 
 
 def setUpModule(module):
-
-    environ_processes = module.environ_processes
+#    environ_processes = module.environ_processes
     config = module.config
-
-    # glance.
-    environ_processes.append(GlanceRegistryProcess(
-            config.glance.directory,
-            config.glance.registry_config))
-    environ_processes.append(GlanceApiProcess(
-            config.glance.directory,
-            config.glance.api_config,
-            config.glance.host,
-            config.glance.port))
-
-    # keystone.
-    environ_processes.append(KeystoneProcess(
-            config.keystone.directory,
-            config.keystone.config,
-            config.keystone.host,
-            config.keystone.port))
-
-    # quantum.
-    environ_processes.append(QuantumProcess(
-        config.quantum.directory,
-        config.quantum.config))
-    environ_processes.append(QuantumPluginOvsAgentProcess(
-        config.quantum.directory,
-        config.quantum.agent_config))
-
-    for process in environ_processes:
-        process.start()
-    time.sleep(10)
-
-
-def tearDownModule(module):
-    for process in module.environ_processes:
-        process.stop()
-    del module.environ_processes[:]
+    try:
+      subprocess.check_call('bin/nova-manage network create '
+                             '--label=private_1-1 '
+                             '--project_id=1 '
+                             '--fixed_range_v4=10.0.0.0/24 '
+                             '--bridge_interface=br-int '
+                             '--num_networks=1 '
+                             '--network_size=32 ',
+                             cwd=config.nova.directory, shell=True)
+      subprocess.check_call('bin/nova-manage network create '
+                            '--label=private_1-2 '
+                            '--project_id=1 '
+                            '--fixed_range_v4=10.0.1.0/24 '
+                            '--bridge_interface=br-int '
+                            '--num_networks=1 '
+                            '--network_size=32 ',
+                            cwd=config.nova.directory, shell=True)
+      subprocess.check_call('bin/nova-manage network create '
+                            '--label=private_1-3 '
+                            '--project_id=1 '
+                            '--fixed_range_v4=10.0.2.0/24 '
+                            '--bridge_interface=br-int '
+                            '--num_networks=1 '
+                            '--network_size=32 ',
+                            cwd=config.nova.directory, shell=True)
+      subprocess.check_call('bin/nova-manage network create '
+                            '--label=private_2-1 '
+                            '--project_id=2 '
+                            '--fixed_range_v4=10.0.3.0/24 '
+                            '--bridge_interface=br-int '
+                            '--num_networks=1 '
+                            '--network_size=32 ',
+                            cwd=config.nova.directory, shell=True)
+    except Exception:
+       pass
 
 
 class FunctionalTest(unittest.TestCase):
@@ -90,87 +89,32 @@ class FunctionalTest(unittest.TestCase):
         self.os = openstack.Manager(config=self.config)
         self.testing_processes = []
 
-        # nova.
-        self.testing_processes.append(NovaApiProcess(
-                self.config.nova.directory,
-                self.config.nova.host,
-                self.config.nova.port))
-        self.testing_processes.append(NovaComputeProcess(
-                self.config.nova.directory))
-        self.testing_processes.append(NovaNetworkProcess(
-                self.config.nova.directory))
-        self.testing_processes.append(NovaSchedulerProcess(
-                self.config.nova.directory))
-
-        # reset db.
-        subprocess.check_call('mysql -u%s -p%s -e "'
-                              'DROP DATABASE IF EXISTS nova;'
-                              'CREATE DATABASE nova;'
-                              '"' % (
-                                  self.config.mysql.user,
-                                  self.config.mysql.password),
-                              shell=True)
-        subprocess.call('bin/nova-manage db sync',
-                        cwd=self.config.nova.directory, shell=True)
-
-        for process in self.testing_processes:
-            process.start()
-        time.sleep(10)
-
-        # create users.
-        subprocess.check_call('bin/nova-manage user create '
-                              '--name=admin --access=secrete --secret=secrete',
-                              cwd=self.config.nova.directory, shell=True)
-        subprocess.check_call('bin/nova-manage user create '
-                              '--name=demo --access=secrete --secret=secrete',
-                              cwd=self.config.nova.directory, shell=True)
-
-        # create projects.
-        subprocess.check_call('bin/nova-manage project create '
-                              '--project=1 --user=admin',
-                              cwd=self.config.nova.directory, shell=True)
-        subprocess.check_call('bin/nova-manage project create '
-                              '--project=2 --user=demo',
-                              cwd=self.config.nova.directory, shell=True)
-
-        # allocate networks.
-        subprocess.check_call('bin/nova-manage network create '
-                              '--label=private_1-1 '
-                              '--project_id=1 '
-                              '--fixed_range_v4=10.0.0.0/24 '
-                              '--bridge_interface=br-int '
-                              '--num_networks=1 '
-                              '--network_size=32 ',
-                              cwd=self.config.nova.directory, shell=True)
-        subprocess.check_call('bin/nova-manage network create '
-                              '--label=private_1-2 '
-                              '--project_id=1 '
-                              '--fixed_range_v4=10.0.1.0/24 '
-                              '--bridge_interface=br-int '
-                              '--num_networks=1 '
-                              '--network_size=32 ',
-                              cwd=self.config.nova.directory, shell=True)
-        subprocess.check_call('bin/nova-manage network create '
-                              '--label=private_2-1 '
-                              '--project_id=2 '
-                              '--fixed_range_v4=10.0.2.0/24 '
-                              '--bridge_interface=br-int '
-                              '--num_networks=1 '
-                              '--network_size=32 ',
-                              cwd=self.config.nova.directory, shell=True)
-
     def tearDown(self):
-        # kill still existing virtual instances.
-        for line in subprocess.check_output('virsh list --all',
-                                            shell=True).split('\n')[2:-2]:
-            (id, name, state) = line.split()
-            if state == 'running':
-                subprocess.check_call('virsh destroy %s' % id, shell=True)
-            subprocess.check_call('virsh undefine %s' % name, shell=True)
+        print """
 
-        for process in self.testing_processes:
-            process.stop()
-        del self.testing_processes[:]
+        Terminate All Instances
+
+        """
+        try:
+            _, servers= self.os.servers_client.list_servers()
+            print "Servers : %s" % servers
+            for s in servers['servers']:
+                try:
+                  print "Find existing instance %s" % s['id']
+                  resp, body = self.os.servers_client.delete_server(s['id'])
+                  if resp['status'] == '200' or resp['status'] == '202':
+                      self.os.servers_client.wait_for_server_not_exists(s['id'])
+                      time.sleep(5)
+                except Exception as e:
+                  print e
+        except Exception:
+           pass
+        print """
+
+        Cleanup DB
+
+        """
+#        self.output_eventlog()
 
 
 class KeypairsTest(FunctionalTest):
--- medium/tests/test_servers_action.py
+++ medium/tests/test_servers_action.py
@@ -48,41 +48,42 @@ environ_processes = []
 def setUpModule(module):
     environ_processes = module.environ_processes
     config = module.config
+    try:
+      subprocess.check_call('bin/nova-manage network create '
+                             '--label=private_1-1 '
+                             '--project_id=1 '
+                             '--fixed_range_v4=10.0.0.0/24 '
+                             '--bridge_interface=br-int '
+                             '--num_networks=1 '
+                             '--network_size=32 ',
+                             cwd=config.nova.directory, shell=True)
+      subprocess.check_call('bin/nova-manage network create '
+                            '--label=private_1-2 '
+                            '--project_id=1 '
+                            '--fixed_range_v4=10.0.1.0/24 '
+                            '--bridge_interface=br-int '
+                            '--num_networks=1 '
+                            '--network_size=32 ',
+                            cwd=config.nova.directory, shell=True)
+      subprocess.check_call('bin/nova-manage network create '
+                            '--label=private_1-3 '
+                            '--project_id=1 '
+                            '--fixed_range_v4=10.0.2.0/24 '
+                            '--bridge_interface=br-int '
+                            '--num_networks=1 '
+                            '--network_size=32 ',
+                            cwd=config.nova.directory, shell=True)
+      subprocess.check_call('bin/nova-manage network create '
+                            '--label=private_2-1 '
+                            '--project_id=2 '
+                            '--fixed_range_v4=10.0.3.0/24 '
+                            '--bridge_interface=br-int '
+                            '--num_networks=1 '
+                            '--network_size=32 ',
+                            cwd=config.nova.directory, shell=True)
+    except Exception:
+       pass
 
-    # glance.
-    environ_processes.append(GlanceRegistryProcess(
-            config.glance.directory,
-            config.glance.registry_config))
-    environ_processes.append(GlanceApiProcess(
-            config.glance.directory,
-            config.glance.api_config,
-            config.glance.host,
-            config.glance.port))
-
-    # keystone.
-    environ_processes.append(KeystoneProcess(
-            config.keystone.directory,
-            config.keystone.config,
-            config.keystone.host,
-            config.keystone.port))
-
-    # quantum.
-    environ_processes.append(QuantumProcess(
-        config.quantum.directory,
-        config.quantum.config))
-    environ_processes.append(QuantumPluginOvsAgentProcess(
-        config.quantum.directory,
-        config.quantum.agent_config))
-
-    for process in environ_processes:
-        process.start()
-    time.sleep(10)
-
-
-def tearDownModule(module):
-    for process in module.environ_processes:
-        process.stop()
-    del module.environ_processes[:]
 
 
 class FunctionalTest(unittest.TestCase):
@@ -97,87 +98,32 @@ class FunctionalTest(unittest.TestCase):
         self.os2 = openstack.Manager(config=self.config2)
         self.testing_processes = []
 
-        # nova.
-        self.testing_processes.append(NovaApiProcess(
-                self.config.nova.directory,
-                self.config.nova.host,
-                self.config.nova.port))
-        self.testing_processes.append(NovaComputeProcess(
-                self.config.nova.directory))
-        self.testing_processes.append(NovaNetworkProcess(
-                self.config.nova.directory))
-        self.testing_processes.append(NovaSchedulerProcess(
-                self.config.nova.directory))
-
-        # reset db.
-        subprocess.check_call('mysql -u%s -p%s -e "'
-                              'DROP DATABASE IF EXISTS nova;'
-                              'CREATE DATABASE nova;'
-                              '"' % (
-                                  self.config.mysql.user,
-                                  self.config.mysql.password),
-                              shell=True)
-        subprocess.check_call('bin/nova-manage db sync',
-                        cwd=self.config.nova.directory, shell=True)
-
-        for process in self.testing_processes:
-            process.start()
-        time.sleep(10)
-
-        # create users.
-        subprocess.check_call('bin/nova-manage user create '
-                              '--name=admin --access=secrete --secret=secrete',
-                              cwd=self.config.nova.directory, shell=True)
-        subprocess.check_call('bin/nova-manage user create '
-                              '--name=demo --access=secrete --secret=secrete',
-                              cwd=self.config.nova.directory, shell=True)
-
-        # create projects.
-        subprocess.check_call('bin/nova-manage project create '
-                              '--project=1 --user=admin',
-                              cwd=self.config.nova.directory, shell=True)
-        subprocess.check_call('bin/nova-manage project create '
-                              '--project=2 --user=demo',
-                              cwd=self.config.nova.directory, shell=True)
-
-        # allocate networks.
-        subprocess.check_call('bin/nova-manage network create '
-                              '--label=private_1-1 '
-                              '--project_id=1 '
-                              '--fixed_range_v4=10.0.0.0/24 '
-                              '--bridge_interface=br-int '
-                              '--num_networks=1 '
-                              '--network_size=32 ',
-                              cwd=self.config.nova.directory, shell=True)
-        subprocess.check_call('bin/nova-manage network create '
-                              '--label=private_1-2 '
-                              '--project_id=1 '
-                              '--fixed_range_v4=10.0.1.0/24 '
-                              '--bridge_interface=br-int '
-                              '--num_networks=1 '
-                              '--network_size=32 ',
-                              cwd=self.config.nova.directory, shell=True)
-        subprocess.check_call('bin/nova-manage network create '
-                              '--label=private_2-1 '
-                              '--project_id=2 '
-                              '--fixed_range_v4=10.0.2.0/24 '
-                              '--bridge_interface=br-int '
-                              '--num_networks=1 '
-                              '--network_size=32 ',
-                              cwd=self.config.nova.directory, shell=True)
 
     def tearDown(self):
-        # kill still existing virtual instances.
-        for line in subprocess.check_output('virsh list --all',
-                                            shell=True).split('\n')[2:-2]:
-            (id, name, state) = line.split()
-            if state == 'running':
-                subprocess.check_call('virsh destroy %s' % id, shell=True)
-            subprocess.check_call('virsh undefine %s' % name, shell=True)
+        print """
 
-        for process in self.testing_processes:
-            process.stop()
-        del self.testing_processes[:]
+        Terminate All Instances
+
+        """
+        try:
+            _, servers= self.os.servers_client.list_servers()
+            print "Servers : %s" % servers
+            for s in servers['servers']:
+                try:
+                  print "Find existing instance %s" % s['id']
+                  resp, body = self.os.servers_client.delete_server(s['id'])
+                  if resp['status'] == '200' or resp['status'] == '202':
+                      self.os.servers_client.wait_for_server_not_exists(s['id'])
+                      time.sleep(5)
+                except Exception as e:
+                  print e
+        except Exception:
+           pass
+        print """
+
+        Cleanup DB
+
+        """
 
     def exec_sql(self, sql):
         exec_sql = 'mysql -u %s -p%s nova -e "' + sql + '"'
@@ -1252,8 +1198,9 @@ class ServersActionTest(FunctionalTest):
         """
         # Make snapshot of the instance.
         alt_name = rand_name('a' * 260)
-        resp, _ = self.ss_client.create_image(test_id, alt_name)
+        resp, body = self.ss_client.create_image(test_id, alt_name)
         self.assertEquals('400', resp['status'])
+        print body
 
     @attr(kind='medium')
     def test_create_image_when_metadata_exists(self):
