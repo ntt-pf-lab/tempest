54b433c03fdbb0198b7464432e6fe80a2bd064b8
Nachi Ueno <ueno.nachi@lab.ntt.co.jp>
2011-12-20 16:37:43 +0900

    Speeding up testcases for test_tenant

--- medium/tests/test_tenant.py
+++ medium/tests/test_tenant.py
@@ -35,16 +35,21 @@ def setUpModule(module):
     environ_processes = module.environ_processes
     config = module.config
 
-    # keystone.
-    environ_processes.append(KeystoneProcess(
-            config.keystone.directory,
-            config.keystone.config,
-            config.keystone.host,
-            config.keystone.port))
+    try:
+        # create users.
+        subprocess.check_call('bin/nova-manage user create '
+                              '--name=%s --access=secrete --secret=secrete'\
+                                      % config.nova.username,
+                              cwd=config.nova.directory, shell=True)
 
-    for process in environ_processes:
-        process.start()
-    time.sleep(10)
+        # create projects.
+        subprocess.check_call('bin/nova-manage project create '
+                              '--project=1 --user=%s'\
+                                      % config.nova.username,
+                              cwd=config.nova.directory, shell=True)
+
+    except Exception:
+        pass
 
 tearDownModule = tearDownModule
 
@@ -59,52 +64,6 @@ class TenantTest(unittest.TestCase):
         self.rest_client = self.os.servers_client.client
 
         self.testing_processes = []
-
-        # nova.
-        self.testing_processes.append(NovaApiProcess(
-                self.config.nova.directory,
-                self.config.nova.host,
-                self.config.nova.port))
-
-        # reset db.
-        subprocess.check_call('mysql -u%s -p%s -e "'
-                              'DROP DATABASE IF EXISTS nova;'
-                              'CREATE DATABASE nova;'
-                              '"' % (
-                                  self.config.mysql.user,
-                                  self.config.mysql.password),
-                              shell=True)
-        subprocess.call('bin/nova-manage db sync',
-                        cwd=self.config.nova.directory, shell=True)
-
-        for process in self.testing_processes:
-            process.start()
-        time.sleep(10)
-
-        # create users.
-        subprocess.check_call('bin/nova-manage user create '
-                              '--name=%s --access=secrete --secret=secrete'\
-                                      % self.config.nova.username,
-                              cwd=self.config.nova.directory, shell=True)
-
-        # create projects.
-        subprocess.check_call('bin/nova-manage project create '
-                              '--project=1 --user=%s'\
-                                      % self.config.nova.username,
-                              cwd=self.config.nova.directory, shell=True)
-
-    def tearDown(self):
-        # kill still existing virtual instances.
-        for line in subprocess.check_output('virsh list --all',
-                                            shell=True).split('\n')[2:-2]:
-            (id, name, state) = line.split()
-            if state == 'running':
-                subprocess.check_call('virsh destroy %s' % id, shell=True)
-            subprocess.check_call('virsh undefine %s' % name, shell=True)
-
-        for process in self.testing_processes:
-            process.stop()
-        del self.testing_processes[:]
 
     @attr(kind='medium')
     def test_A00_01_version(self):
