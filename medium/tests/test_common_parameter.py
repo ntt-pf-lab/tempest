82171bddde625a49a3f4081365245ac9cb664014
Nachi Ueno <ueno.nachi@lab.ntt.co.jp>
2011-12-20 19:07:42 +0900

    Speeding up testcases for test_common_parameter

--- medium/tests/test_common_parameter.py
+++ medium/tests/test_common_parameter.py
@@ -50,41 +50,55 @@ def setUpModule(module):
     environ_processes = module.environ_processes
     config = module.config
 
-    # glance.
-    environ_processes.append(GlanceRegistryProcess(
-            config.glance.directory,
-            config.glance.registry_config))
-    environ_processes.append(GlanceApiProcess(
-            config.glance.directory,
-            config.glance.api_config,
-            config.glance.host,
-            config.glance.port))
+    try:
+        # create users.
+        subprocess.check_call('bin/nova-manage user create '
+                              '--name=admin --access=secrete --secret=secrete',
+                              cwd=config.nova.directory, shell=True)
+        subprocess.check_call('bin/nova-manage user create '
+                              '--name=demo --access=secrete --secret=secrete',
+                              cwd=config.nova.directory, shell=True)
 
-    # keystone.
-    environ_processes.append(KeystoneProcess(
-            config.keystone.directory,
-            config.keystone.config,
-            config.keystone.host,
-            config.keystone.port))
+        # create projects.
+        subprocess.check_call('bin/nova-manage project create '
+                              '--project=1 --user=admin',
+                              cwd=config.nova.directory, shell=True)
+        subprocess.check_call('bin/nova-manage project create '
+                              '--project=2 --user=demo',
+                              cwd=config.nova.directory, shell=True)
 
-    # quantum.
-    environ_processes.append(QuantumProcess(
-        config.quantum.directory,
-        config.quantum.config))
-    environ_processes.append(QuantumPluginOvsAgentProcess(
-        config.quantum.directory,
-        config.quantum.agent_config))
+        # allocate networks.
+        subprocess.check_call('bin/nova-manage network create '
+                              '--label=private_1-1 '
+                              '--project_id=1 '
+                              '--fixed_range_v4=10.0.0.0/24 '
+                              '--bridge_interface=br-int '
+                              '--num_networks=1 '
+                              '--network_size=32 ',
+                              cwd=config.nova.directory, shell=True)
+        subprocess.check_call('bin/nova-manage network create '
+                              '--label=private_1-2 '
+                              '--project_id=1 '
+                              '--fixed_range_v4=10.0.1.0/24 '
+                              '--bridge_interface=br-int '
+                              '--num_networks=1 '
+                              '--network_size=32 ',
+                              cwd=config.nova.directory, shell=True)
+        subprocess.check_call('bin/nova-manage network create '
+                              '--label=private_2-1 '
+                              '--project_id=2 '
+                              '--fixed_range_v4=10.0.2.0/24 '
+                              '--bridge_interface=br-int '
+                              '--num_networks=1 '
+                              '--network_size=32 ',
+                              cwd=config.nova.directory, shell=True)
 
-    for process in environ_processes:
-        process.start()
-    time.sleep(10)
+    except Exception:
+        pass
 
 
 def tearDownModule(module):
-    for process in module.environ_processes:
-        process.stop()
-    time.sleep(10)
-    del module.environ_processes[:]
+    pass
 
 
 class FunctionalTest(unittest.TestCase):
@@ -95,76 +109,6 @@ class FunctionalTest(unittest.TestCase):
         self.os = openstack.Manager(config=self.config)
         self.testing_processes = []
 
-        # nova.
-        self.testing_processes.append(NovaApiProcess(
-                self.config.nova.directory,
-                self.config.nova.host,
-                self.config.nova.port))
-        self.testing_processes.append(NovaComputeProcess(
-                self.config.nova.directory))
-        self.testing_processes.append(NovaNetworkProcess(
-                self.config.nova.directory))
-        self.testing_processes.append(NovaSchedulerProcess(
-                self.config.nova.directory))
-
-        # reset db.
-        subprocess.check_call('mysql -u%s -p%s -e "'
-                              'DROP DATABASE IF EXISTS nova;'
-                              'CREATE DATABASE nova;'
-                              '"' % (
-                                  self.config.mysql.user,
-                                  self.config.mysql.password),
-                              shell=True)
-        subprocess.call('bin/nova-manage db sync',
-                        cwd=self.config.nova.directory, shell=True)
-
-        time.sleep(0)
-        for process in self.testing_processes:
-            process.start()
-        time.sleep(10)
-
-        # create users.
-        subprocess.check_call('bin/nova-manage user create '
-                              '--name=admin --access=secrete --secret=secrete',
-                              cwd=self.config.nova.directory, shell=True)
-        subprocess.check_call('bin/nova-manage user create '
-                              '--name=demo --access=secrete --secret=secrete',
-                              cwd=self.config.nova.directory, shell=True)
-
-        # create projects.
-        subprocess.check_call('bin/nova-manage project create '
-                              '--project=1 --user=admin',
-                              cwd=self.config.nova.directory, shell=True)
-        subprocess.check_call('bin/nova-manage project create '
-                              '--project=2 --user=demo',
-                              cwd=self.config.nova.directory, shell=True)
-
-        # allocate networks.
-        subprocess.check_call('bin/nova-manage network create '
-                              '--label=private_1-1 '
-                              '--project_id=1 '
-                              '--fixed_range_v4=10.0.0.0/24 '
-                              '--bridge_interface=br-int '
-                              '--num_networks=1 '
-                              '--network_size=32 ',
-                              cwd=self.config.nova.directory, shell=True)
-        subprocess.check_call('bin/nova-manage network create '
-                              '--label=private_1-2 '
-                              '--project_id=1 '
-                              '--fixed_range_v4=10.0.1.0/24 '
-                              '--bridge_interface=br-int '
-                              '--num_networks=1 '
-                              '--network_size=32 ',
-                              cwd=self.config.nova.directory, shell=True)
-        subprocess.check_call('bin/nova-manage network create '
-                              '--label=private_2-1 '
-                              '--project_id=2 '
-                              '--fixed_range_v4=10.0.2.0/24 '
-                              '--bridge_interface=br-int '
-                              '--num_networks=1 '
-                              '--network_size=32 ',
-                              cwd=self.config.nova.directory, shell=True)
-
     def tearDown(self):
         # kill still existing virtual instances.
         for line in subprocess.check_output('virsh list --all',
@@ -173,11 +117,6 @@ class FunctionalTest(unittest.TestCase):
             if state == 'running':
                 subprocess.check_call('virsh destroy %s' % id, shell=True)
             subprocess.check_call('virsh undefine %s' % name, shell=True)
-
-        for process in self.testing_processes:
-            process.stop()
-        del self.testing_processes[:]
-        time.sleep(10)
 
     def exec_sql(self, sql):
         exec_sql = 'mysql -u %s -p%s nova -e "' + sql + '"'
