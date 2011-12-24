b83b7f2c73a8262c8987d68368ced369fcf310ab
Nachi Ueno <ueno.nachi@lab.ntt.co.jp>
2011-12-24 11:06:07 -0800

    fixed error code in run_lt_tests

--- run_lt_tests.sh
+++ run_lt_tests.sh
@@ -2,15 +2,41 @@
 
 exec_date=`date '+%Y%m%d%H%M'`
 pwd_place=`pwd`
-echo [$pwd_place] 
-nosetests -s ./medium/tests/test_flavors.py 2>&1 |tee -a medium/tests/$exec_date.log
-nosetests -s ./medium/tests/test_images.py 2>&1 |tee -a medium/tests/$exec_date.log
-nosetests -s ./medium/tests/test_keypairs.py 2>&1 |tee -a medium/tests/$exec_date.log
-nosetests -s ./medium/tests/test_servers_action.py 2>&1 |tee -a medium/tests/$exec_date.log
-nosetests -s ./medium/tests/test_servers.py 2>&1 |tee -a medium/tests/$exec_date.log
-nosetests -s ./medium/tests/test_tenant.py 2>&1 |tee -a medium/tests/$exec_date.log
-nosetests -s ./medium/tests/test_through.py 2>&1 |tee -a medium/tests/$exec_date.log
-nosetests -s ./medium/tests/test_virtual_interfaces.py 2>&1 |tee -a medium/tests/$exec_date.log
+EXIT_CODE=0
+echo [$pwd_place]
+nosetests -s ./medium/tests/test_flavors.py
+if [ $? != 0 ]; then
+    EXIT_CODE=1
+fi
+nosetests -s ./medium/tests/test_images.py
+if [ $? != 0 ]; then
+    EXIT_CODE=1
+fi
+nosetests -s ./medium/tests/test_keypairs.py
+if [ $? != 0 ]; then
+    EXIT_CODE=1
+fi
+nosetests -s ./medium/tests/test_servers_action.py
+if [ $? != 0 ]; then
+    EXIT_CODE=1
+fi
+nosetests -s ./medium/tests/test_servers.py
+if [ $? != 0 ]; then
+    EXIT_CODE=1
+fi
+nosetests -s ./medium/tests/test_tenant.py
+if [ $? != 0 ]; then
+    EXIT_CODE=1
+fi
+nosetests -s ./medium/tests/test_through.py
+if [ $? != 0 ]; then
+    EXIT_CODE=1
+fi
+osetests -s ./medium/tests/test_virtual_interfaces.py
+if [ $? != 0 ]; then
+    EXIT_CODE=1
+fi
 
+exit $EXIT_CODE
 
 
