7a39762a62b1b157dbd434f4903281544801a265
Nachi Ueno <ueno.nachi@lab.ntt.co.jp>
2011-12-25 16:04:33 +0900

    fixed exit code in run_lt_tests

--- run_lt_tests.sh
+++ run_lt_tests.sh
@@ -5,35 +5,35 @@ pwd_place=`pwd`
 EXIT_CODE=0
 echo [$pwd_place]
 nosetests -s ./medium/tests/test_flavors.py
-if [ $? != 0 ]; then
+if [ "$?" -ne "0" ]; then
     EXIT_CODE=1
 fi
 nosetests -s ./medium/tests/test_images.py
-if [ $? != 0 ]; then
+if [ "$?" -ne "0" ]; then
     EXIT_CODE=1
 fi
 nosetests -s ./medium/tests/test_keypairs.py
-if [ $? != 0 ]; then
+if [ "$?" -ne "0" ]; then
     EXIT_CODE=1
 fi
 nosetests -s ./medium/tests/test_servers_action.py
-if [ $? != 0 ]; then
+if [ "$?" -ne "0" ]; then
     EXIT_CODE=1
 fi
 nosetests -s ./medium/tests/test_servers.py
-if [ $? != 0 ]; then
+if [ "$?" -ne "0" ]; then
     EXIT_CODE=1
 fi
 nosetests -s ./medium/tests/test_tenant.py
-if [ $? != 0 ]; then
+if [ "$?" -ne "0" ]; then
     EXIT_CODE=1
 fi
 nosetests -s ./medium/tests/test_through.py
-if [ $? != 0 ]; then
+if [ "$?" -ne "0" ]; then
     EXIT_CODE=1
 fi
-osetests -s ./medium/tests/test_virtual_interfaces.py
-if [ $? != 0 ]; then
+nosetests -s ./medium/tests/test_virtual_interfaces.py
+if [ "$?" -ne "0" ]; then
     EXIT_CODE=1
 fi
 
