137f0c6afbb0a6e0b07d0aeb820ee928be7842a9
openstack <openstack@ubuntu.(none)>
2011-12-26 15:45:04 +0900

    Remove initialize network codes.

--- medium/tests/test_images.py
+++ medium/tests/test_images.py
@@ -94,39 +94,6 @@ def setUpModule(module):
                               'Member user3 tenant2',
                               cwd=config.keystone.directory, shell=True)
 
-        # allocate networks.
-        subprocess.check_call('/opt/openstack/nova/bin/nova-manage network create '
-                               '--label=private_1-1 '
-                               '--project_id=1 '
-                               '--fixed_range_v4=10.0.0.0/24 '
-                               '--bridge_interface=br-int '
-                               '--num_networks=1 '
-                               '--network_size=32 ',
-                               cwd=config.nova.directory, shell=True)
-        subprocess.check_call('/opt/openstack/nova/bin/nova-manage network create '
-                              '--label=private_1-2 '
-                              '--project_id=1 '
-                              '--fixed_range_v4=10.0.1.0/24 '
-                              '--bridge_interface=br-int '
-                              '--num_networks=1 '
-                              '--network_size=32 ',
-                              cwd=config.nova.directory, shell=True)
-        subprocess.check_call('/opt/openstack/nova/bin/nova-manage network create '
-                              '--label=private_1-3 '
-                              '--project_id=1 '
-                              '--fixed_range_v4=10.0.2.0/24 '
-                              '--bridge_interface=br-int '
-                              '--num_networks=1 '
-                              '--network_size=32 ',
-                              cwd=config.nova.directory, shell=True)
-        subprocess.check_call('/opt/openstack/nova/bin/nova-manage network create '
-                              '--label=private_2-1 '
-                              '--project_id=2 '
-                              '--fixed_range_v4=10.0.3.0/24 '
-                              '--bridge_interface=br-int '
-                              '--num_networks=1 '
-                              '--network_size=32 ',
-                              cwd=config.nova.directory, shell=True)
     except Exception:
         pass
 
--- medium/tests/test_servers.py
+++ medium/tests/test_servers.py
@@ -41,41 +41,6 @@ environ_processes = []
 def setUpModule(module):
 #    environ_processes = module.environ_processes
     config = module.config
-    try:
-        subprocess.check_call('/opt/openstack/nova/bin/nova-manage network create '
-                               '--label=private_1-1 '
-                               '--project_id=1 '
-                               '--fixed_range_v4=10.0.0.0/24 '
-                               '--bridge_interface=br-int '
-                               '--num_networks=1 '
-                               '--network_size=32 ',
-                               cwd=config.nova.directory, shell=True)
-        subprocess.check_call('/opt/openstack/nova/bin/nova-manage network create '
-                              '--label=private_1-2 '
-                              '--project_id=1 '
-                              '--fixed_range_v4=10.0.1.0/24 '
-                              '--bridge_interface=br-int '
-                              '--num_networks=1 '
-                              '--network_size=32 ',
-                              cwd=config.nova.directory, shell=True)
-        subprocess.check_call('/opt/openstack/nova/bin/nova-manage network create '
-                              '--label=private_1-3 '
-                              '--project_id=1 '
-                              '--fixed_range_v4=10.0.2.0/24 '
-                              '--bridge_interface=br-int '
-                              '--num_networks=1 '
-                              '--network_size=32 ',
-                              cwd=config.nova.directory, shell=True)
-        subprocess.check_call('/opt/openstack/nova/bin/nova-manage network create '
-                              '--label=private_2-1 '
-                              '--project_id=2 '
-                              '--fixed_range_v4=10.0.3.0/24 '
-                              '--bridge_interface=br-int '
-                              '--num_networks=1 '
-                              '--network_size=32 ',
-                              cwd=config.nova.directory, shell=True)
-    except Exception:
-        pass
 
 
 class FunctionalTest(unittest.TestCase):
