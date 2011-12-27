6b14921e36e12e8b46936b3739ffd3ce9df21b8c
Nachi Ueno <ueno.nachi@lab.ntt.co.jp>
2011-12-27 12:03:46 +0900

    fixed run_lt_tests

--- run_lt_tests.sh
+++ run_lt_tests.sh
@@ -4,8 +4,35 @@ exec_date=`date '+%Y%m%d%H%M'`
 pwd_place=`pwd`
 EXIT_CODE=0
 echo [$pwd_place]
-nosetests -s ./medium/tests/test_flavors.py ./medium/tests/test_images.py ./medium/tests/test_keypairs.py ./medium/tests/test_servers_action.py ./medium/tests/test_servers.py ./medium/tests/test_tenant.py ./medium/tests/test_through.py ./medium/tests/test_virtual_interfaces.py
-
+nosetests -v -s ./medium/tests/test_flavors.py
+if [ "$?" -ne "0" ]; then
+    EXIT_CODE=1
+fi
+nosetests -v -s ./medium/tests/test_images.py
+if [ "$?" -ne "0" ]; then
+    EXIT_CODE=1
+fi
+nosetests -v -s ./medium/tests/test_keypairs.py
+if [ "$?" -ne "0" ]; then
+    EXIT_CODE=1
+fi
+nosetests -v -s ./medium/tests/test_servers_action.py
+if [ "$?" -ne "0" ]; then
+    EXIT_CODE=1
+fi
+nosetests -v -s ./medium/tests/test_servers.py
+if [ "$?" -ne "0" ]; then
+    EXIT_CODE=1
+fi
+nosetests -v -s ./medium/tests/test_tenant.py
+if [ "$?" -ne "0" ]; then
+    EXIT_CODE=1
+fi
+nosetests -v -s ./medium/tests/test_through.py
+if [ "$?" -ne "0" ]; then
+    EXIT_CODE=1
+fi
+nosetests -v -s ./medium/tests/test_virtual_interfaces.py
 if [ "$?" -ne "0" ]; then
     EXIT_CODE=1
 fi
