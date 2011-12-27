f37e7e36cd399922ccdd1112f48b485b0ab89008
Nachi Ueno <ueno.nachi@lab.ntt.co.jp>
2011-12-27 15:51:18 +0900

    add init network script to run_lt_tests.sh

--- run_lt_tests.sh
+++ run_lt_tests.sh
@@ -1,5 +1,19 @@
 #!/usr/bin/env bash
 
+
+# allocate networks.
+#mysql -uroot -pnova nova -e "delete from fixed_ips;"
+#mysql -uroot -pnova nova -e "delete from networks;"
+#mysql -uroot -pnova nova -e "delete from virtual_interfaces;"
+#mysql -uroot -pnova ovs_quantum -e "delete from networks;"
+
+cmd=/opt/openstack/nova/bin/nova-manage
+#cmd=/opt/stack/nova/bin/nova-manage
+$cmd network create --label=private_1-1 --project_id=1 --fixed_range_v4=10.0.0.0/24 --bridge_interface=br-int --num_networks= --network_size=32
+$cmd network create --label=private_1-2 --project_id=1 --fixed_range_v4=10.0.1.0/24 --bridge_interface=br-int --num_networks= --network_size=32
+$cmd network create --label=private_1-3 --project_id=1 --fixed_range_v4=10.0.2.0/24 --bridge_interface=br-int --num_networks= --network_size=32
+$cmd network create --label=private_2-1 --project_id=2 --fixed_range_v4=10.0.3.0/24 --bridge_interface=br-int --num_networks= --network_size=32
+
 exec_date=`date '+%Y%m%d%H%M'`
 pwd_place=`pwd`
 EXIT_CODE=0
