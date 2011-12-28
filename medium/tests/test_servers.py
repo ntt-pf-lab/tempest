0c51496620b937f581a31bd8abf078b37a30ffbb
Nachi Ueno <ueno.nachi@lab.ntt.co.jp>
2011-12-28 00:59:54 +0000

    fixed test_create_server_quota_memory

--- medium/tests/test_servers.py
+++ medium/tests/test_servers.py
@@ -2602,7 +2602,7 @@ class ServersTest(FunctionalTest):
         """
 
         sql = ('INSERT INTO quotas(deleted, project_id, resource, hard_limit)'
-               "VALUES(0, '2', 'ram', 1024)")
+               "VALUES(0, '1', 'ram', 1024)")
         self.exec_sql(sql)
 
         print """
@@ -2654,6 +2654,8 @@ class ServersTest(FunctionalTest):
         print "resp=", resp
         print "server=", server
         self.assertEqual('413', resp['status'])
+        sql = "delete from quotas;"
+        self.exec_sql(sql)
 
     @test.skip_test('ignore this case for bug.619')
     @attr(kind='medium')
