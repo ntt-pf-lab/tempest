335e86a895e384a93b1ad78f2592d8f2980cdd31
Rohit Karajgi <rohit.karajgi@vertex.co.in>
2012-01-18 10:54:13 -0800

    Added tests that call Stackmonkey API through processes.py in the run_lt_tests.sh script

--- run_lt_tests.sh
+++ run_lt_tests.sh
@@ -56,6 +56,35 @@ if [ "$?" -ne "0" ]; then
     EXIT_CODE=1
 fi
 
+nosetests -v -s
+./medium/tests/test_opposite_system_illegal_state_create_image.py
+if [ "$?" -ne "0" ]; then
+    EXIT_CODE=1
+fi
+
+nosetests -v -s
+./medium/tests/test_opposite_system_illegal_state_create_server.py
+if [ "$?" -ne "0" ]; then
+    EXIT_CODE=1
+fi
+
+nosetests -v -s
+./medium/tests/test_opposite_system_illegal_state_delete_server.py
+if [ "$?" -ne "0" ]; then
+    EXIT_CODE=1
+fi
+
+nosetests -v -s
+./medium/tests/test_opposite_system_illegal_state_reboot_server.py
+if [ "$?" -ne "0" ]; then
+    EXIT_CODE=1
+fi
+
+nosetests -v -s ./medium/tests/test_process_down.py
+if [ "$?" -ne "0" ]; then
+    EXIT_CODE=1
+fi
+
 exit $EXIT_CODE
 
 
