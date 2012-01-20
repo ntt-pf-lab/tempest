cfef20026402a00a7ad25e54dc2790e38088a97c
Hisaharu Ishii <ishii.hisaharu@lab.ntt.co.jp>
2012-01-20 11:50:58 -0800

    restore wrong line delete in commit 655a231ae4f53ff37bf2c95885335160946c4402

--- storm/common/rest_client.py
+++ storm/common/rest_client.py
@@ -15,6 +15,7 @@ class RestClient(object):
             self.token, self.base_url = self.keystone_v2_auth(user,
                                                               key,
                                                               auth_url,
+                                                              tenant_name)
         else:
             self.token, self.base_url = self.basic_auth(user,
                                                         key,
