e0f862d33967ca4579e18a45c3bbd67505d1693b
Takahiro Shida <shida@intellilink.co.jp>
2012-01-22 04:51:04 -0800

    Add long term, velocity, scenario testcases

--- large/tests/test_scenario.py
+++ large/tests/test_scenario.py
@@ -86,9 +86,13 @@ class FunctionalTest(unittest.TestCase):
         config.keystone.conf.set('keystone', 'user', user)
         config.keystone.conf.set('keystone', 'password', password)
         config.keystone.conf.set('keystone', 'tenant_name', tenant_name)
-        self._load_client(config)
+        config.nova.conf.set('nova', 'user', user)
+        config.nova.conf.set('nova', 'api_key', password)
+        config.nova.conf.set('nova', 'tenant_name', tenant_name)
+        
+        self._load_client(config, data=self.data)
 
-    def _load_client(self, config):
+    def _load_client(self, config, data=None):
         self.os = openstack.Manager(config)
         self.keystone_client = self.os.keystone_client
         self.token_client = TokenClient(config)
@@ -100,6 +104,7 @@ class FunctionalTest(unittest.TestCase):
         token = self.token_client.get_token(config.keystone.user,
                                             config.keystone.password,
                                             config.keystone.tenant_name)        
+        self.nova_manage_network = NetworkWrapper(self.default_config)
         self.glance = GlanceWrapper(token, self.default_config)
         self.data = DataGenerator(self.keystone_client,
                                   self.token_client,
@@ -107,7 +112,35 @@ class FunctionalTest(unittest.TestCase):
                                   self.server_client,
                                   self.images_client,
                                   self.quantum_client,
-                                  self.glance)
+                                  self.nova_manage_network,
+                                  self.glance,
+                                  data=data)
+
+    def _run_instance(self, image):
+        meta = {}
+        accessIPv4 = ''
+        accessIPv6 = ''
+        name = "senario1"
+        flavor_ref = '1'
+        file_contents = 'This is a test file.'
+        personality = [{'path': '/etc/test.txt',
+                       'contents': base64.b64encode(file_contents)}]
+        _, server = self.server_client.create_server(name,
+                                                    image,
+                                                    flavor_ref,
+                                                    meta=meta,
+                                                    accessIPv4=accessIPv4,
+                                                    accessIPv6=accessIPv6,
+                                                    personality=personality)
+
+        # Wait for the server to become active
+        self.server_client.wait_for_server_status(server['id'], 'ACTIVE')
+        return server['id']
+
+    def _terminate_instance(self, server_id):
+        self.server_client.delete_server(server_id)
+        self.server_client.wait_for_server_not_exists_ignore_error(server_id)
+
 
 class DBController(object):
     def __init__(self, config):
@@ -132,7 +165,9 @@ class DataGenerator(object):
                        server_client,
                        images_client,
                        quantum_client,
-                       glance):
+                       nova_manage_network,
+                       glance,
+                       data=None):
         self.keystone_client = keystone_client
         self.token_client = token_client
         self.keypair_client = keypair_client
@@ -147,15 +182,19 @@ class DataGenerator(object):
         self.keypairs = []
         self.images = []
         self.l2 = []
-        
+        self.nw = []
+        self.nova_manage_network = nova_manage_network
+        self.data = data
 
-    def setup_one_user(self):
-        _, tenant = self.keystone_client.create_tenant(TENANT, "tenant_for_test")
-        _, user = self.keystone_client.create_user(USER, PASSWORD, tenant['tenant']['id'], USER + "@mail.com")
-        self.tenants.append(tenant['tenant'])
-        self.users.append(user['user'])
+    def setup_one_user(self, name):
+        _, tenant = self.keystone_client.create_tenant(name + '_tenant', "tenant_for_test")
+        _, user = self.keystone_client.create_user(name, PASSWORD, tenant['tenant']['id'], name + "@mail.com")
+        self.tenants.append(tenant['tenant']['id'])
+        self.users.append(user['user']['id'])
+        return {'user': user['user'], 'tenant': tenant['tenant']}
 
     def setup_role(self):
+        result = []
         _, services = self.keystone_client.get_services()
         services = services['OS-KSADM:services']
         for service in services:
@@ -164,15 +203,21 @@ class DataGenerator(object):
             _, roles = self.keystone_client.create_role(service_name + ':test1' , 'Test role', service_id)
             self.role_name = service_name + ':test1'
             self.roles.append(roles['role'])
+            result.append(roles['role'])
+        return {'role':result}
 
-    def add_image(self, image_name, image_format, container_format, image_file):
-        image_id = self.glance.add(image_name, image_format, container_format, image_file)
+    def add_image(self, image_name, image_format, container_format, image_file, kernel_id=None):
+        if kernel_id:
+            image_id = self.glance.add_image(image_name, image_format, container_format, image_file, kernel_id)
+        else:
+            image_id = self.glance.add(image_name, image_format, container_format, image_file)            
         self.images.append(image_id)
         return image_id
 
     def add_keypair(self, keypair_name):
         self.keypair_client.create_keypair(keypair_name)
         self.keypairs.append(keypair_name)
+        return {'keypair': keypair_name}
 
     def create_network(self, name):
         _, body =self.quantum_client.create_network("test_network", 'nova_id')
@@ -180,28 +225,35 @@ class DataGenerator(object):
         self.l2.append(network['id'])
         return network['id']
 
+    def create_ip_block(self, label, ip_range, size, bridge, tenant, uuid, gw, dhcp):
+        self.nova_manage_network.create_network(label, ip_range, size, bridge, tenant, uuid, gw, dhcp)
+        self.nw.append(uuid)
+        return uuid
+
     def teardown_all(self):
-        for u in self.users:
-            self.keystone_client.delete_user(u['id'])
-        for t in self.tenants:
-            self.keystone_client.delete_tenant(t['id'])
-        for r in self.roles:
-            self.keystone_client.delete_role(r['id'])
         for i in self.images:
             self.glance.delete(i)
         for k in self.keypairs:
             self.keypair_client.delete_keypair(k)
-#        for n in self.l2:
-#            self.quantum_client.delete_network(n)
-
+        for n in self.nw:
+            self.nova_manage_network.delete_network(n)
+        for u in self.users:
+            self.keystone_client.delete_user(u)
+        for t in self.tenants:
+            self.keystone_client.delete_tenant(t)
+        for r in self.roles:
+            self.keystone_client.delete_role(r['id'])
+        if self.data:
+            self.data.teardown_all()
 
 class NetworkWrapper(object):
     def __init__(self, config):
         self.path = config.nova.directory
-    
+
     def _nova_manage_network(self, action, params):
-        cmd = "bin/nova-manage network %s %s" % (action, params)
-        print "Running command %s" % cmd
+        flags = "--flagfile=etc/nova.conf"
+        cmd = "bin/nova-manage %s network %s %s" % (flags, action, params)
+#        print "Running command %s" % cmd
         result = subprocess.check_output(cmd, cwd=self.path, shell=True)
         return result
 
@@ -227,7 +279,7 @@ class GlanceWrapper(object):
     def _glance(self, action, params, yes=None):
         cmd = "glance -A %s -H %s -p %s %s %s" %\
              (self.token, self.host, self.port, action, params)
-        print "Running command %s" % cmd
+#        print "Running command %s" % cmd
         if yes:
             cmd = ("yes %s|" % yes) + cmd
         result = subprocess.check_output(cmd, cwd=self.path, shell=True)
@@ -249,6 +301,20 @@ class GlanceWrapper(object):
             splited = str(result).split()
             return splited[splited.count(splited)-1]
 
+    def add_image(self, image_name, image_format, container_format, image_file, kernel_id):
+        params = "name=%s is_public=true disk_format=%s container_format=%s kernel_id=%s < %s" \
+                      % (image_name,
+                        image_format,
+                        container_format,
+                        kernel_id,
+                        os.path.join(os.getcwd(), image_file))
+        result = self._glance('add', params)
+        # parse add new image ID: <image_id>
+        if result:
+            splited = str(result).split()
+            return splited[splited.count(splited)-1]
+
+
     def delete(self, image_id):
         result = self._glance('delete', image_id, yes="y")
         if result:
@@ -266,42 +332,124 @@ class GlanceWrapper(object):
         
 class ScenarioTest(FunctionalTest):
 
-    def test_scenario_check_setups(self):
-        # create new uses
-        self.data.setup_one_user()
-        self.data.setup_role()
-        # create glance image.
-        token = self.token_client.get_token(USER, PASSWORD, TENANT)
-        glance = GlanceWrapper(token, self.default_config)
-        new_image_id = glance.add("test_scenario1", "ami", "ami", "etc/images/tty.img")
-        print new_image_id
-        # check setuped image.
-        print glance.index()
-        print glance.detail('test_scenario1')
-        print glance.update(new_image_id, "updated_image01")
-        print glance.detail('updated_image01')
-        # remove image.
-        print glance.delete(new_image_id)
-        # remove user and tenants in teardown.
-
     def _application_tenant(self, scenario):
-        results = []
+        results = {}
         # create new uses
-        self.data.setup_one_user()
-        self.data.setup_role()
+        results.update(self.data.setup_one_user(scenario))
+        self.swap_user(scenario, PASSWORD, scenario + "_tenant")
         # create glance image.
-        fw_image = self.data.add_image(scenario + "_FW", 'ami', 'ami', "etc/images/tty.img")
-        lb_image = self.data.add_image(scenario + "_LB", 'ami', 'ami', "etc/images/tty.img")
-        server_image = self.data.add_image(scenario + "_Server", 'ami', 'ami', "etc/images/tty.img")
+        kernel = self.data.add_image(scenario + "_Kernel", 'aki', 'aki', "etc/images/ttylinux-vmlinuz")
+        results.update({'Kernel Image': kernel})
+        
+        fw_image = self.data.add_image(scenario + "_FW", 'ami', 'ami', "etc/images/tty.img", kernel)
+        results.update({'FW Image': fw_image})
+        lb_image = self.data.add_image(scenario + "_LB", 'ami', 'ami', "etc/images/tty.img", kernel)
+        results.update({'LB Image': lb_image})
+        server_image = self.data.add_image(scenario + "_Server", 'ami', 'ami', "etc/images/tty.img", kernel)
+        results.update({'Server Image': server_image})
         # create keypair
-        self.data.add_keypair(scenario + "_keypair")
+        key = self.data.add_keypair(scenario + "_keypair")
+        results.update({'Keypair': key})
         # create L2 network.
-        self.data.create_network(scenario)
+        nw = self.data.create_network(scenario)
+        results.update({'L2 Network': nw})
+        # create IP block.
+        block = self.data.create_ip_block(scenario, '10.1.1.0/24', 255, 'virbr0', results['tenant']['id'], nw, '10.1.1.255', '10.1.1.2')
+        results.update({'IP Block': block})
+        return results
         
     def test_scenario_application_new_tenant(self):
-        uuid = self.data.create_network('test')
-        wrapper = NetworkWrapper(self.default_config)
-        wrapper.create_network('test1', '10.1.3.0/24', '256', 'br_int', 'prjAdmin', uuid, '10.1.3.255', '10.1.3.2')
+        '''
+        Scenario test for create "Applicate new tenant"
+        1. Create new user/tenant
+        2. Authorize
+        3. Create keypair for user
+        4. Create network for tenant
+        5. Boot 3 instance for user
+        '''
+        try:
+            setups = self._application_tenant("scenario_application")
+        except:
+            self.fail('Failed to setup tenant info')
+        try:
+            # create instances.
+            vmfw = self._run_instance(setups['FW Image'])
+            vmlb = self._run_instance(setups['LB Image'])
+            vmserver = self._run_instance(setups['Server Image'])
+        except:
+            self.fail('Failed to create server.')
+        finally:
+            self._terminate_instance(vmfw)
+            self._terminate_instance(vmlb)
+            self._terminate_instance(vmserver)
 
-#        self._application_tenant("scenario1")
-        
+    def test_scenario_scaleout(self):
+        '''
+        Scenario test for create "Add VM/Scale out"
+        1. Applicate new tenant
+          1.1. Create new user/tenant
+          1.2. Authorize
+          1.3. Create keypair for user
+          1.4. Create network for tenant
+          1.5. Boot 3 instance for user
+        2. Bootup new vm
+          2.1. Boot 1 instance for user
+        '''
+        try:
+            setups = self._application_tenant("scenario_scaleout")
+        except:
+            self.fail('Failed to setup tenant info')
+        try:
+            # create instances.
+            vmfw = self._run_instance(setups['FW Image'])
+            vmlb = self._run_instance(setups['LB Image'])
+            vmserver = self._run_instance(setups['Server Image'])
+            scaled = self._run_instance(setups['Server Image'])
+        except:
+            self.fail('Failed to create server.')
+        finally:
+            self._terminate_instance(scaled)
+            self._terminate_instance(vmserver)
+            self._terminate_instance(vmlb)
+            self._terminate_instance(vmfw)
+
+    def test_scenario_snapshot(self):
+        '''
+        Scenario test for create "Snapshot"
+        1. Applicate new tenant
+          1.1. Create new user/tenant
+          1.2. Authorize
+          1.3. Create keypair for user
+          1.4. Create network for tenant
+          1.5. Boot 3 instance for user
+        2. Snapshot VM(Server)
+          2.1. Snapshot to VM(Server)
+        '''
+        try:
+            setups = self._application_tenant("scenario_snapshot")
+        except:
+            self.fail('Failed to setup tenant info')
+        try:
+            # create instances.
+            vmfw = self._run_instance(setups['FW Image'])
+            vmlb = self._run_instance(setups['LB Image'])
+            vmserver = self._run_instance(setups['Server Image'])
+            try:
+                # create snapshot.
+                resp, _ = self.server_client.create_image(vmserver, 'snapshot')
+                alt_img_url = resp['location']
+                match = re.search('/images/(?P<image_id>.+)', alt_img_url)
+                self.assertIsNotNone(match)
+                alt_img_id = match.groupdict()['image_id']
+                self.img_client.wait_for_image_status(alt_img_id, 'ACTIVE')
+            except:
+                self.fail('Failed to snapshot.')
+            finally:
+                self.glance.delete(alt_img_id)
+        except:
+            self.fail('Failed to create server.')
+        finally:
+            self._terminate_instance(vmfw)
+            self._terminate_instance(vmlb)
+            self._terminate_instance(vmserver)
+
