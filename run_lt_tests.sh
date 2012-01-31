39f0f9ea2a23852aff9609e6c452c8c4f1f03a12
Hisaharu Ishii <ishii.hisaharu@lab.ntt.co.jp>
2012-01-31 14:35:59 -0800

    fix message in run_lt_tests.sh

--- run_lt_tests.sh
+++ run_lt_tests.sh
@@ -28,17 +28,17 @@ EXIT_CODE=0
 echo [$pwd_place]
 
 for test_case in $@; do
-    printf "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
-    printf "& Start MT TestSet %-20s &" $test_case
-    printf "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
+    printf "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&\n"
+    printf "& Start MT TestSet %-20s &\n" $test_case
+    printf "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&\n"
     start_sec=`date +%s`
     nosetests -v -s ./medium/tests/test_${test_case}.py
     if [ "$?" -ne "0" ]; then
         EXIT_CODE=1
     fi
-    printf "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
-    printf "& Finished MT TestSet %-20s in %05d sec &" $test_case $((`date +%s` - $s))
-    printf "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
+    printf "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&\n"
+    printf "& Finished MT TestSet %-20s in %05d sec &\n" $test_case $((`date +%s` - $s))
+    printf "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&\n"
 done
 
 exit $EXIT_CODE
