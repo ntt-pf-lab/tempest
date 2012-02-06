dd75a296c5c8177126851291c1083ff9086105f8
Takahiro Shida <shida@intellilink.co.jp>
2012-02-05 23:14:51 -0800

    specify flag file when execute nova-manage command

--- ci_setup.sh
+++ ci_setup.sh
@@ -1,24 +1,19 @@
 #!/usr/bin/env bash
 
-if [ $# -eq 0 ]; then
-    set 'flavors keypairs reboot_server create_image create_server delete_update_server list_server tenant through virtual_interfaces images'
-#commeted out: set 'servers_action servers servers2'
-fi
-
-# allocate networks.
 #mysql -uroot -pnova nova -e "delete from fixed_ips;"
 #mysql -uroot -pnova nova -e "delete from networks;"
 #mysql -uroot -pnova nova -e "delete from virtual_interfaces;"
 #mysql -uroot -pnova ovs_quantum -e "delete from networks;"
-
-cmd=/opt/openstack/nova/bin/nova-manage
+source /opt/openstack/os-inst-m2/install.conf
+cmd=$NOVA_HOME/bin/nova-manage
+flags=$NOVA_HOME/etc/nova.conf
 #cmd=/opt/stack/nova/bin/nova-manage
-$cmd network create --label=private_1-1 --project_id=1 --fixed_range_v4=10.0.0.0/24 --bridge_interface=br-int --num_networks= --network_size=32
-$cmd network create --label=private_1-2 --project_id=1 --fixed_range_v4=10.0.1.0/24 --bridge_interface=br-int --num_networks= --network_size=32
-$cmd network create --label=private_1-3 --project_id=1 --fixed_range_v4=10.0.2.0/24 --bridge_interface=br-int --num_networks= --network_size=32
-$cmd network create --label=private_2-1 --project_id=2 --fixed_range_v4=10.0.3.0/24 --bridge_interface=br-int --num_networks= --network_size=32
+$cmd --flagfile=$flags network create --label=private_1-1 --project_id=1 --fixed_range_v4=10.0.0.0/24 --bridge_interface=br-int --num_networks= --network_size=32
+$cmd --flagfile=$flags network create --label=private_1-2 --project_id=1 --fixed_range_v4=10.0.1.0/24 --bridge_interface=br-int --num_networks= --network_size=32
+$cmd --flagfile=$flags network create --label=private_1-3 --project_id=1 --fixed_range_v4=10.0.2.0/24 --bridge_interface=br-int --num_networks= --network_size=32
+$cmd --flagfile=$flags network create --label=private_2-1 --project_id=2 --fixed_range_v4=10.0.3.0/24 --bridge_interface=br-int --num_networks= --network_size=32
 
-keystone=/opt/openstack/keystone/bin/keystone-manage
+keystone=$KEYSTONE_HOME/bin/keystone-manage
 $keystone tenant add "prjTest"
 $keystone user add usrTest passwordTest
 $keystone role grant Member usrTest prjTest
--- run_lt_tests.sh
+++ run_lt_tests.sh
@@ -4,44 +4,6 @@ if [ $# -eq 0 ]; then
     set 'flavors keypairs reboot_server create_image create_server delete_update_server list_server tenant through virtual_interfaces images'
 #commeted out: set 'servers_action servers servers2'
 fi
-
-# allocate networks.
-#mysql -uroot -pnova nova -e "delete from fixed_ips;"
-#mysql -uroot -pnova nova -e "delete from networks;"
-#mysql -uroot -pnova nova -e "delete from virtual_interfaces;"
-#mysql -uroot -pnova ovs_quantum -e "delete from networks;"
-
-cmd=/opt/openstack/nova/bin/nova-manage
-#cmd=/opt/stack/nova/bin/nova-manage
-$cmd network create --label=private_1-1 --project_id=1 --fixed_range_v4=10.0.0.0/24 --bridge_interface=br-int --num_networks= --network_size=32
-$cmd network create --label=private_1-2 --project_id=1 --fixed_range_v4=10.0.1.0/24 --bridge_interface=br-int --num_networks= --network_size=32
-$cmd network create --label=private_1-3 --project_id=1 --fixed_range_v4=10.0.2.0/24 --bridge_interface=br-int --num_networks= --network_size=32
-$cmd network create --label=private_2-1 --project_id=2 --fixed_range_v4=10.0.3.0/24 --bridge_interface=br-int --num_networks= --network_size=32
-
-keystone=/opt/openstack/keystone/bin/keystone-manage
-$keystone tenant add "prjTest"
-$keystone user add usrTest passwordTest
-$keystone role grant Member usrTest prjTest
-
-exec_date=`date '+%Y%m%d%H%M'`
-pwd_place=`pwd`
-EXIT_CODE=0
-echo [$pwd_place]
-
-for test_case in $@; do
-    printf "#########################################\n"
-    printf "# Start MT TestSet %-20s #\n" $test_case
-    printf "#########################################\n"
-    start_sec=`date +%s`
-    nosetests -v -s ./medium/tests/test_${test_case}.py
-    if [ "$?" -ne "0" ]; then
-        EXIT_CODE=1
-    fi
-    printf "########################################################\n"
-    printf "# Finished MT TestSet %-20s in %05d sec #\n" $test_case $((`date +%s` - $start_sec))
-    printf "########################################################\n"
-done
-
-exit $EXIT_CODE
-
+./ci_setup.sh
+./ci_run_test.sh
 
