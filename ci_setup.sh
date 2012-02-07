24d406f59e714665b50fe7dbc2d335b0170f7686
Hisaharu Ishii <ishii.hisaharu@lab.ntt.co.jp>
2012-02-07 11:56:44 -0800

    move default testset definition from run_lt_tests to ci_run_test.sh. refactor --flag-file option in ci_setup.sh. Feature #1141

--- ci_setup.sh
+++ ci_setup.sh
@@ -5,13 +5,12 @@
 #mysql -uroot -pnova nova -e "delete from virtual_interfaces;"
 #mysql -uroot -pnova ovs_quantum -e "delete from networks;"
 source /opt/openstack/os-inst-m2/install.conf
-cmd=$NOVA_HOME/bin/nova-manage
-flags=$NOVA_HOME/etc/nova.conf
-#cmd=/opt/stack/nova/bin/nova-manage
-$cmd --flagfile=$flags network create --label=private_1-1 --project_id=1 --fixed_range_v4=10.0.0.0/24 --bridge_interface=br-int --num_networks= --network_size=32
-$cmd --flagfile=$flags network create --label=private_1-2 --project_id=1 --fixed_range_v4=10.0.1.0/24 --bridge_interface=br-int --num_networks= --network_size=32
-$cmd --flagfile=$flags network create --label=private_1-3 --project_id=1 --fixed_range_v4=10.0.2.0/24 --bridge_interface=br-int --num_networks= --network_size=32
-$cmd --flagfile=$flags network create --label=private_2-1 --project_id=2 --fixed_range_v4=10.0.3.0/24 --bridge_interface=br-int --num_networks= --network_size=32
+flags=$NOVA_ETC_DIR/nova.conf
+cmd=$NOVA_HOME/bin/nova-manage --flagfile=$flags
+$cmd network create --label=private_1-1 --project_id=1 --fixed_range_v4=10.0.0.0/24 --bridge_interface=br-int --num_networks= --network_size=32
+$cmd network create --label=private_1-2 --project_id=1 --fixed_range_v4=10.0.1.0/24 --bridge_interface=br-int --num_networks= --network_size=32
+$cmd network create --label=private_1-3 --project_id=1 --fixed_range_v4=10.0.2.0/24 --bridge_interface=br-int --num_networks= --network_size=32
+$cmd network create --label=private_2-1 --project_id=2 --fixed_range_v4=10.0.3.0/24 --bridge_interface=br-int --num_networks= --network_size=32
 
 keystone=$KEYSTONE_HOME/bin/keystone-manage
 $keystone tenant add "prjTest"
--- run_lt_tests.sh
+++ run_lt_tests.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-if [ $# -eq 0 ]; then
-    set 'flavors keypairs reboot_server create_image create_server delete_update_server list_server tenant through virtual_interfaces images'
-#commeted out: set 'servers_action servers servers2'
-fi
 ./ci_setup.sh
-./ci_run_test.sh
+./ci_run_test.sh $@
 
