777b8d5c25075fa9fd43b3277933dca658c9799c
Takahiro Shida <shida@intellilink.co.jp>
2012-02-27 02:46:12 -0800

    Remove unused network creation

--- ci_setup.sh
+++ ci_setup.sh
@@ -7,8 +7,6 @@
 source /opt/openstack/os-inst-m2/install.conf
 flags=$NOVA_ETC_DIR/nova.conf
 cmd="$NOVA_HOME/bin/nova-manage --flagfile=$flags"
-$cmd network create --label=private_1-1 --project_id=1 --fixed_range_v4=10.0.0.0/24 --bridge_interface=br-int --num_networks= --network_size=32 --gateway=10.0.0.1
-$cmd network create --label=private_1-2 --project_id=1 --fixed_range_v4=10.0.1.0/24 --bridge_interface=br-int --num_networks= --network_size=32 --gateway=10.0.1.1
 $cmd network create --label=private_1-3 --project_id=1 --fixed_range_v4=10.0.2.0/24 --bridge_interface=br-int --num_networks= --network_size=32 --gateway=10.0.2.1
 $cmd network create --label=private_2-1 --project_id=2 --fixed_range_v4=10.0.3.0/24 --bridge_interface=br-int --num_networks= --network_size=32 --gateway=10.0.3.1
 
